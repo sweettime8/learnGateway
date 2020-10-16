7fa46c7cab04931b261f3cec1eb41edf0257e23e
unknown <nghduc91@gmail.com>
2020-10-16 16:18:07 +0700

    first commit

---
+++ src/main/java/com/elcom/gateway/ElcomServiceApplication.java
@@ -0,0 +1,20 @@
+package com.elcom.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+//@EnableCaching
+//@EnableScheduling
+//@EnableDiscoveryClient
+public class ElcomServiceApplication {
+    
+    public static void main(String[] args) {
+        // Fix lỗi "UDP failed setting ip_ttl | Method not implemented" khi start app trên Windows
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        
+        SpringApplication.run(ElcomServiceApplication.class, args);
+    }
+}
